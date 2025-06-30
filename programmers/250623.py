9be7821393f9c0bc920092967fe3cca373172f79
kkaeunii <kaeun562@gamil.com>
2025-06-30 23:24:00 +0900

    feat : Algorithm study

---
+++ programmers/250623.py
@@ -0,0 +1,22 @@
+#%%
+# 문제1 : 몫 구하기
+# 풀이
+def solution(num1, num2):
+    answer = num1 // num2
+    return answer
+
+# 프로그래머스 다른 사람 풀이(맘에 든 풀이)
+def solution(num1, num2):
+    answer = num1 / num2
+    return int(answer)
+
+# 다른 사람 풀이(처음 본 메서드)
+solution = int.__floordiv__
+
+#%%
+# 문제2 : 피자 나눠먹기(1)
+# 풀이
+
+#%%
+# 문제3 : 개미군단
+# 풀이
