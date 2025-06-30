a07479ee7c3688c9f3e3bd1f2ba4cf5f45066b45
kkaeunii <kaeun562@gamil.com>
2025-06-30 22:14:31 +0900

    feat : Algorithm study

---
+++ programmers/250617.py
@@ -0,0 +1,15 @@
+#%%
+# 문제1 : 두 수의 차 구하기
+# 풀이
+def solution(num1, num2):
+    answer = num1 - num2
+    return answer
+
+#%%
+# 문제2 : 배열 원소의 길이
+
+#%%
+# 문제3 : 편지
+def solution(strlist):
+    answer = strlist
+    return len(answer)
