7ed7ec518bfab88e88ceb168be7fe2e48365f5c6
jkim1209 <jwk143@gmail.com>
2025-07-23 01:08:06 +0900

    feat: Add Problem 4

--- 0721.py
+++ 0721.py
@@ -32,7 +32,45 @@ def solution(arr, query):
 
 
 #%%
-# Problem 4. Lv.2 모음사전
-# https://school.programmers.co.kr/learn/courses/30/lessons/84512
+# Problem 4. Lv.0 정수를 나선형으로 배치하기
+# https://school.programmers.co.kr/learn/courses/30/lessons/181832
 # Solution
+def solution(n):
+    arrs = [[0] * n for _ in range(n)]
+    x, y = -1, 0
+    val = 1
+    length = n
+    
+    for i in range(n**2):
+        for _ in range(length):    
+            if i%4 == 0: 
+                x+=1
+                len_change = True
+            elif i%4 == 1: 
+                y+=1
+                len_change = False
+            elif i%4 == 2: 
+                x-=1
+                len_change = True
+            elif i%4 == 3:
+                y-=1
+                len_change = False
+            
+            arrs[y][x] = val
+            val += 1
+        if len_change: length -= 1
+    return arrs
 
+# Other Solution
+def solution(n):
+    answer = [[None for j in range(n)] for i in range(n)]
+    move = [[0, 1], [1, 0], [0, -1], [-1, 0]]
+    x, y, m = 0, 0, 0
+    for i in range(1, n**2 + 1):
+        answer[y][x] = i
+        if y + move[m][0] >= n or x + move[m][1] >= n or answer[y + move[m][0]][x + move[m][1]]:
+            m = (m + 1) % len(move)
+        y, x = y + move[m][0], x + move[m][1]
+    return answer
+
+# Ref. 0619.py Lv.2 삼각달팽이
